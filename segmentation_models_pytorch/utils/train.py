d6bb5e3ea2531877f371e2ba1688987e666be8c1
Julien Maille <julien.maille@gmail.com>
2020-03-27 00:21:32 +0100

    Epoch hanfle image filename and compute per-class metric

--- segmentation_models_pytorch/utils/train.py
+++ segmentation_models_pytorch/utils/train.py
@@ -40,11 +40,12 @@ class Epoch:
         logs = {}
         loss_meter = AverageValueMeter()
         metrics_meters = {metric.__name__: AverageValueMeter() for metric in self.metrics}
+        metrics_class_meters = {metric.__name__+'-c{}'.format(cls): AverageValueMeter() for metric in self.metrics for cls in range(2) }
 
         with tqdm(dataloader, desc=self.stage_name, file=sys.stdout, disable=not (self.verbose), leave=not (self.verbose)) as iterator:
-            for x, y in iterator:
+            for x, y, fname in iterator:
                 x, y = x.to(self.device), y.to(self.device)
-                loss, y_pred = self.batch_update(x, y)
+                loss, y_pred = self.batch_update(x, y, fname)
 
                 # update loss logs
                 loss_value = loss.cpu().detach().numpy()
@@ -54,10 +55,15 @@ class Epoch:
 
                 # update metrics logs
                 for metric_fn in self.metrics:
-                    metric_value = metric_fn(y_pred, y).cpu().detach().numpy()
-                    metrics_meters[metric_fn.__name__].add(metric_value)
+                    for cls in range(2):
+                        metric_class = metric_fn(y_pred[:, cls, :, :].unsqueeze(1), y[:, cls, :, :].unsqueeze(1)).cpu().detach().numpy()
+                        metrics_class_meters[metric_fn.__name__+'-c{}'.format(cls)].add(metric_class)
+                    metric_total = metric_fn(y_pred, y).cpu().detach().numpy()
+                    metrics_meters[metric_fn.__name__].add(metric_total)
                 metrics_logs = {k: v.mean for k, v in metrics_meters.items()}
+                metrics_class_logs = {k: v.mean for k, v in metrics_class_meters.items()}
                 logs.update(metrics_logs)
+                logs.update(metrics_class_logs)
 
                 if self.verbose:
                     s = self._format_logs(logs)
@@ -82,7 +88,7 @@ class TrainEpoch(Epoch):
     def on_epoch_start(self):
         self.model.train()
 
-    def batch_update(self, x, y):
+    def batch_update(self, x, y, fname):
         self.optimizer.zero_grad()
         prediction = self.model.forward(x)
         loss = self.loss(prediction, y)
